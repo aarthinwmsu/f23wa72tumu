3af97992670ecebd0654434b507f954d42479c25
aarthinwmsu <s563072@nwmissouri.edu>
2023-10-23 12:38:12 -0500

    Updated with functions in Computation endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -6,7 +6,7 @@ var log = Math.log(rand);
 var log10 = Math.log10(rand);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('computation', { title: `Function imul applied to ${rand} is ${imul} '\n' Function log applied to ${rand} is ${log} '\n' Function log10 applied to ${rand} is ${log10}` });
+  res.render('computation', { title: `Function imul applied to ${rand} is ${imul}, Function log applied to ${rand} is ${log}, Function log10 applied to ${rand} is ${log10}` });
 });
 
 module.exports = router;
