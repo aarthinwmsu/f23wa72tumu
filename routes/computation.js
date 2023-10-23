1cbd3d28ee981b27a94157e7fbfb9d233bffeb2f
aarthinwmsu <s563072@nwmissouri.edu>
2023-10-23 12:45:14 -0500

    Updated computation endpoint with query argument

--- routes/computation.js
+++ routes/computation.js
@@ -6,6 +6,10 @@ var log = Math.log(rand);
 var log10 = Math.log10(rand);
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var x = req.query.x;
+  if(x!=null){
+    rand=x;
+  }
   res.render('computation', { title: `Function imul applied to ${rand} is ${imul}, Function log applied to ${rand} is ${log}, Function log10 applied to ${rand} is ${log10}` });
 });
 
